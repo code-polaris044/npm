0f70de53d53bf63fad06c5736f52c6c30ccd3933
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-06-19 21:56:45 +0900

    api表示

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,28 @@
-const a = "DDD";
-console.log(a);
+let a = fetch(
+  "https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare"
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((response) => {
+    const data = response.data; // dataオブジェクトを取得
 
-let shun = {
-  name: "菅原",
-  pro: function () {},
-};
+    console.log(response); // { success: true, data: { ... } }
+    console.log("Name:", data.name);
+    console.log("Age:", data.age);
+    console.log("Note:", data.note);
+    console.log("Register Date:", data.registerDate);
 
-shun.name = "俊哉";
-shun.ago = 20;
+    document.getElementById("music").innerText = `
+      Name: ${data.name}
+      Age: ${data.age}
+      Note: ${data.note}
+      Register Date: ${data.registerDate}
+    `;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
